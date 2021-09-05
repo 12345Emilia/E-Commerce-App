25bb12683717bb9f5df43764782fee570bf0c3f0
Emilia <emilia.assarsson@hotmail.com>
2021-09-04 21:52:24 -0600

    Added login, product, register pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import Home from './pages/Home';
-import ProductList from './pages/ProductList';
+// import Home from './pages/Home';
+// import Product from './pages/Product';
+// import ProductList from './pages/ProductList';
+import Cart from './pages/Cart';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
 
 const App = () => {
-  return <ProductList />;
+  return <Cart />;
 };
 export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url('https://www.tripsavvy.com/thmb/FwmQ-JvBEBDDlVb-j_zdEo0iVsA=/2048x1152/smart/filters:no_upscale()/beach-5b59c9b7c9e77c004b3e0ff0.jpg');
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+`;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0px;
+  padding: 10px;
+`;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  margin-bottom: 10px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>Sign In</Title>
+        <Form>
+          <Input placeholder='usename' />
+          <Input placeholder='password' />
+          <Button>LOGIN</Button>
+          <Link>Forgot password</Link>
+          <Link>Create a new account</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
