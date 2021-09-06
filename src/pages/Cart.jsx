7a8c5ba58a1bdfa0bfda2d77fced00763c8adae7
Emilia <emilia.assarsson@hotmail.com>
2021-09-05 18:29:45 -0600

    Added responsive

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,15 @@
+import { Add, Remove } from '@material-ui/icons';
 import styled from 'styled-components';
 import Announcement from '../components/Announcement';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import { mobile } from '../responsive';
 
 const Container = styled.div``;
+
 const Wrapper = styled.div`
   padding: 20px;
+  ${mobile({ padding: '10px' })}
 `;
 const Title = styled.h1`
   font-weight: 300;
@@ -17,7 +21,10 @@ const Top = styled.div`
   justify-content: space-between;
   padding: 20px;
 `;
-const TopTexts = styled.div``;
+const TopTexts = styled.div`
+  ${mobile({ display: 'none' })}
+`;
+
 const TopText = styled.span`
   text-decoration: underline;
   cursor: pointer;
@@ -32,8 +39,102 @@ const TopButton = styled.button`
     props.type === 'filled' ? 'black' : 'transparent'};
   color: ${(props) => props.type === 'filled' && 'white'};
 `;
-const Bottom = styled.div``;
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: 'column' })}
+`;
+const Info = styled.div`
+  flex: 3;
+`;
 
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: 'column' })}
+`;
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+const Image = styled.img`
+  width: 200px;
+`;
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+const ProductName = styled.span``;
+
+const ProductId = styled.span``;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+const ProductSize = styled.span``;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+  ${mobile({ margin: '5px 15px' })}
+`;
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+  ${mobile({ marginBottom: '20px' })}
+`;
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === 'total' && '500'};
+  font-size: ${(props) => props.type === 'total' && '24px'};
+`;
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
 const Cart = () => {
   return (
     <Container>
@@ -49,7 +150,81 @@ const Cart = () => {
           </TopTexts>
           <TopButton type='filled'>Checkout Now</TopButton>
         </Top>
-        <Bottom></Bottom>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src='https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/8978d034-93ec-4e28-825d-5732cdf81304/wearallday-womens-shoes-knM7Ct.png' />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>Emma Shoe
+                  </ProductName>
+                  <ProductId>
+                    <b>ID: </b>91234512
+                  </ProductId>
+                  <ProductColor color='black' />
+                  <ProductSize>
+                    <b>Size: </b>38
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src='https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fd4%2Fe6%2Fd4e6a32adc57c8d01f5515f07fc1a2fcab3c1ea4.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/main]' />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>Top
+                  </ProductName>
+                  <ProductId>
+                    <b>ID: </b>91234512
+                  </ProductId>
+                  <ProductColor color='pink' />
+                  <ProductSize>
+                    <b>Size: </b>M
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 14</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>Order Summary</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 75</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 6.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -6.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type='total'>
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 75</SummaryItemPrice>
+            </SummaryItem>
+            <Button>Complete Purchase</Button>
+          </Summary>
+        </Bottom>
       </Wrapper>
       <Footer />
     </Container>
